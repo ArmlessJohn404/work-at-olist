f5be88a72e0b7794bfa1d11a2b647e1e7706addb
Luiz Eduardo Amaral <luizamaral306@gmail.com>
2016-11-23 16:01:48 -0200

    Created Node data strtucture

--- work-at-olist/api/models.py
+++ work-at-olist/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# TODO: Implement Node as an "ltree" or "mtptt" make queries log(n)
+class Node(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey('self', blank=True, null=True)
+
+    class Meta:
+        unique_together = (("name", "parent"))
+
+    def __repr__(self):
+        return self.tree
+
+    @property
+    def parent_name(self):
+        return self.parent.name if isinstance(self.parent, Node) else 'root'
+
+    @property
+    def tree(self):
+        if isinstance(self.parent, Node):
+            return self.parent.tree+" / "+self.name
+        return self.name
